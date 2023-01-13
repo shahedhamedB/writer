52c89071f15e29526e0a649d6dea624b5cad3eae
shahedhamedB <37702676+shahedhamedB@users.noreply.github.com>
2023-01-13 18:26:23 +0300

    add home

--- app/build/tmp/kapt3/stubs/debug/com/example/dynamodb/core/ExtensionsKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/dynamodb/core/ExtensionsKt.java
@@ -2,7 +2,7 @@ package com.example.dynamodb.core;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 2, d1 = {"\u0000\u0012\n\u0000\n\u0002\u0010\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u001e\u0010\u0000\u001a\u00020\u0001*\u00020\u00022\u0012\u0010\u0003\u001a\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00010\u0004\u00a8\u0006\u0005"}, d2 = {"onClick", "", "Landroid/view/View;", "callback", "Lkotlin/Function1;", "app_debug"})
+@kotlin.Metadata(mv = {1, 7, 1}, k = 2, d1 = {"\u0000\u0012\n\u0000\n\u0002\u0010\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u001e\u0010\u0000\u001a\u00020\u0001*\u00020\u00022\u0012\u0010\u0003\u001a\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00010\u0004\u00a8\u0006\u0005"}, d2 = {"onClick", "", "Landroid/view/View;", "callback", "Lkotlin/Function1;", "app_debug"})
 public final class ExtensionsKt {
     
     public static final void onClick(@org.jetbrains.annotations.NotNull()
